78954d891af94f13089571c41f7c42045f5f7b2b
Alam Alonzo <alonzo.alam@gmail.com>
2024-11-25 12:16:54 -0400

    Initial Commit

---
+++ RickAndMortySDP/RickAndMortySDPApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortySDPApp.swift
+//  RickAndMortySDP
+//
+//  Created by Alam Alonzo on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortySDPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
